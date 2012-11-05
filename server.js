955bb64d3d3584eda8913af0aeee39764d11e2a9
Viktor Jucas <viktor.jucas@gmail.com>
2012-11-05 11:03:27 -0600

    Add router.

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var http = require("http");
+var url = require("url");
 
-function start() {
+function start(route) {
 	function onRequest(request, response) {
-		console.log("Request received.");
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received.");
 		response.writeHead(200, {"Content-Type": "text/plain"});
+
+		route(pathname);
+
 		response.write("Hello World");
 		response.end();
 	}
