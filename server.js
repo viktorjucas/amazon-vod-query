57afd819ae25112c04b5e0de6766601e5e5298b4
Viktor Jucas <viktor.jucas@gmail.com>
2012-11-05 11:20:02 -0600

    Adding requestHandlers without responses.

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var http = require("http");
 var url = require("url");
 
-function start(route) {
+function start(route, handle) {
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
+
+		route(handle, pathname);
+
 		response.writeHead(200, {"Content-Type": "text/plain"});
-
-		route(pathname);
-
 		response.write("Hello World");
 		response.end();
 	}
